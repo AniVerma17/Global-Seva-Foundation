022325a472fd5155f14865c67a5ec81c2597b7e9
dishantsethi <dishantsethi14@gmail.com>
2019-01-27 00:15:10 +0530

    fucked

--- globalseva/globalseva/views.py
+++ globalseva/globalseva/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 
 def about(request):
     return render(request, 'about.html')
-    # return HttpResponse('about')
 
 def homepage(request):
     return render(request, 'homepage.html')
